7ad29cf39fb1c7d4e46e0207065c4553ffd6c569
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-04-05 22:02:33 -0300

    title

--- ri_lab_01/spiders/brasil_elpais.py
+++ ri_lab_01/spiders/brasil_elpais.py
@@ -17,15 +17,30 @@ class BrasilElpaisSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+
+    def parse_urls(self, response):
+        for artigo in response.css('div.articulo__interior'):
+            url = artigo.css('h2.articulo-titulo a::attr(href)').get()
+            url = 'https:' + str(url)
+            yield response.follow(url, callback=self.parse_content)
+            
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+        
+        yield response.follow(response.url, callback=self.parse_urls)
+    
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        
+    def parse_content(self, response):
+        for item in response.css('div.contenedor'):
+            title = str(item.css('h1.articulo-titulo::text').get().encode('utf-8'))
+            
+
+            yield {
+                'title': title
+            }
+
+            
